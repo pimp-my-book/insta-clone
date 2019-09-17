708576db365c7705914ac01cfc8d3645319731a4
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-17 17:11:42 +0200

    Add queries

--- insta-backend/src/resolvers/index.js
+++ insta-backend/src/resolvers/index.js
@@ -3,7 +3,9 @@ import { createPost } from "./mutation";
 
 export const resolvers = {
     Query: {
-        hello: (root, args, context) => hello(args, context)
+        hello: (root, args, context) => hello(args, context),
+        getAllPosts: (root, args, context) => getAllPosts(args, context),
+        getUserPost: (root, args, context) => getUserPost(args, context)
     },
     Mutation: {
         createPost: (root, args, context) => createPost(args, context)
