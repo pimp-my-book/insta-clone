f597a83419d1ebbce8b031ed2604b36c779d312b
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-17 17:12:11 +0200

    Add -> getAllPosts query

--- insta-backend/src/resolvers/query.js
+++ insta-backend/src/resolvers/query.js
@@ -3,3 +3,27 @@ import * as dynamodbLib from "../../resources/libs/dynamodb-lib";
 export const hello = (args, context) => {
     return "Your GraphQL API is now LIVE!🎈 "
 }
+
+export const getAllPosts = async (args, context) => {
+    const params = {
+        TableName: process.env.InstaCloneTable
+    }
+    try {
+        const result = await dynamodbLib.call("scan", params);
+        console.log(result);
+        return result.Items.map((item) => (
+            {
+                postId: item.pk,
+                userId: item.sk,
+                caption: item.caption,
+                dateUploaded: item.dateUploaded,
+                postedBy: item.postedBy,
+                imageUrl: item.imageUrl
+            }
+        ))
+    }
+    catch (e) {
+        return e
+    }
+}
+
