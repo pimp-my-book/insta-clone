393dbb9614984e89d0b9871f06003ed6d661ce35
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-17 12:45:45 +0200

    Add mutation section

--- insta-backend/src/resolvers/index.js
+++ insta-backend/src/resolvers/index.js
@@ -4,5 +4,8 @@ import { createPost } from "./mutation";
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
+    },
+    Mutation: {
+        createPost: (root, args, context) => createPost(args, context)
     }
 }
