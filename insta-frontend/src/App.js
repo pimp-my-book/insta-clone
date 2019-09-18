d2f15edd4809134338a5c1d90947b91cab7421f4
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 12:16:04 +0200

    Update -> App.js

--- insta-frontend/src/App.js
+++ insta-frontend/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './styles/styles.css';
+import React, { Component } from "react";
+import { Icon } from "@chakra-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Icon name="check-circle" size="24px" />
+      </div>
+    );
+  }
 }
 
 export default App;
