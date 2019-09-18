45a366411152548ed463d97f72a8c7f85b745f1c
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 14:27:45 +0200

    Import register container

--- insta-frontend/src/Routes.js
+++ insta-frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
+import Register from "./containers/Register";
 
 export default ({ childProps }) =>
     <Switch>
