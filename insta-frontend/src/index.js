e00b8d83b9155aa306370ffdd1912039eec472d4
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 13:20:17 +0200

    Update imports -> index.js

--- insta-frontend/src/index.js
+++ insta-frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import Amplify, { Auth } from 'aws-amplify';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './resources/styles/styles.css';
 import App from './App';
+import config from "./configs/config";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
