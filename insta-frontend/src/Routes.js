dccf3e9bd430353c61c8ef4cbb0429cfe272cf3c
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 17:26:16 +0200

    Add -> CreatePost Container to Routes

--- insta-frontend/src/Routes.js
+++ insta-frontend/src/Routes.js
@@ -3,9 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Register from "./containers/Register";
+import CreatePost from "./containers/CreatePost";
 
 export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={ Home } props={ childProps } />
         <Route path="/Register" exact component={ Register } props={ childProps } />
+        <Route path="/CreatePost" exact component={ CreatePost } props={ childProps } />
     </Switch>
