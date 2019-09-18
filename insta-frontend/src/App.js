f279ea428a9cba4711d6a3675fbfa1b39d0c2b7d
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 13:33:02 +0200

    Add -> Auth handlers

--- insta-frontend/src/App.js
+++ insta-frontend/src/App.js
@@ -4,17 +4,34 @@ import { Box, Heading } from "@chakra-ui/core";
 import Routes from "./Routes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+  
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+    
     return (
       <ThemeProvider>
         <ColorModeProvider>
-          <Box justifyContent="space-between" bg="Black" w="100%" p={ 2 } color="white">
-            <Heading size="md" textAlign="center">
-              Insta-Clone<span role="img" aria-label="Camera">📸</span>
-            </Heading>
-          </Box>
-        <Routes/>
-      </ColorModeProvider>
+            <Box justifyContent="space-between" bg="Black" w="100%" p={ 2 } color="white">
+              <Heading size="md" textAlign="center">
+                Insta-Clone<span role="img" aria-label="Camera">📸</span>
+              </Heading>
+            </Box>
+          <Routes childProps={ childProps }/>
+        </ColorModeProvider>
       </ThemeProvider>
     );
   }
