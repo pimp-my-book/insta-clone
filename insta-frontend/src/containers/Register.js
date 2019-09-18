5096cc3a34c55de8c57029bd4186500f25a9b678
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 14:28:33 +0200

    Register.js ->define react-hooks

--- insta-frontend/src/containers/Register.js
+++ insta-frontend/src/containers/Register.js
@@ -2,3 +2,8 @@ import React, { useState } from "react";
 import Auth from "@aws-amplify/auth";
 import { Box, Heading, Input, Button, FormControl, FormLabel, FormHelperText } from "@chakra-ui/core";
 
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmationCode, setConfirmationCode ] = useState("");
+    const [ newUser, setNewUser ] = useState(null);
+
