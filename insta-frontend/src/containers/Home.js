02948b7722166a23fa71fa53438ab667eae1a32b
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 17:29:48 +0200

    Update imports -> Home container

--- insta-frontend/src/containers/Home.js
+++ insta-frontend/src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Box, Heading } from "@chakra-ui/core";
+import { Box, Link, Heading, Input, Button, FormControl, FormLabel, FormHelperText } from "@chakra-ui/core";
 
 export default class Home extends Component {
     render() {
