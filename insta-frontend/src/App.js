e0c97c0c9220b0e86cbe8ed8861153285390d5f6
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 13:15:04 +0200

    Update -> App.js

--- insta-frontend/src/App.js
+++ insta-frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
+import { Box, Heading } from "@chakra-ui/core";
+import Routes from "./Routes";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Insta-Clone
-      </div>
+      <ThemeProvider>
+        <ColorModeProvider>
+          <Box justifyContent="space-between" bg="Black" w="100%" p={ 2 } color="white">
+            <Heading size="md" textAlign="center">
+              Insta-Clone<span role="img" aria-label="Camera">📸</span>
+            </Heading>
+          </Box>
+        <Routes/>
+      </ColorModeProvider>
+      </ThemeProvider>
     );
   }
 }
