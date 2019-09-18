9e51b129a4413c5d06eca06c780256df98842c4e
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 12:17:57 +0200

    Config -> def resources

--- insta-frontend/src/resources/configs/config.js
+++ insta-frontend/src/resources/configs/config.js
@@ -11,3 +11,16 @@ const dev = {
     }
 };
 
+const prod = {
+    apiGateway: {
+        REGION: process.env.REACT_APP_REGION,
+        URL: process.env.REACT_APP_ENDPNT_PROD
+    },
+    cognito: {
+        REGION: process.env.REACT_APP_REGION,
+        USER_POOL_ID: process.env.REACT_APP_USRPL_ID_PROD,
+        APP_CLIENT_ID: process.env.REACT_APP_USRPL_CLNTID_PROD,
+        IDENTITY_POOL_ID: process.env.REACT_APP_IDPL_ID_PROD
+    }
+};
+
