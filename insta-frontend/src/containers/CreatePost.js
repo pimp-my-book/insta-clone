64e51d9e0612b5435c871c05c162bc41c1f44b83
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-18 17:28:48 +0200

    Add -> Post Upload form

--- insta-frontend/src/containers/CreatePost.js
+++ insta-frontend/src/containers/CreatePost.js
@@ -42,3 +42,63 @@ import config from "../configs/config";
         }
     }
 
+    const Post = () => {
+        return(
+            <Box display="block" w="100%" pr="35%" pl="35%">
+                <Heading>Create Your Post</Heading>
+                    <FormControl size="md"
+                        onSubmit={
+                            async e => {
+                                e.preventDefault();
+                                mutateCreatePost({ variables: {
+                                    postedBy,
+                                    caption,
+                                    imageUrl
+                                }
+                                })
+                            }
+                        }
+                    >
+                        <FormLabel>Caption</FormLabel>
+                            <Input 
+                                type="text" 
+                                id="caption"
+                                value={ caption }
+                                onChange = { e => setCaption( e.target.value ) }
+                                aria-describedby="email-helper-text" />
+                        <FormHelperText id="caption-helper-text">
+                            Because words matter<span role="img" aria-label="Happy">😄</span>
+                        </FormHelperText>
+                        <FormLabel>Who are you?</FormLabel>
+                            <Input 
+                                type="text" 
+                                id="caption"
+                                value={ postedBy }
+                                onChange = { e => setPostedBy( e.target.value ) }
+                                aria-describedby="email-helper-text" />
+                        <FormHelperText id="caption-helper-text">
+                            Because words matter<span role="img" aria-label="Happy">😄</span>
+                        </FormHelperText>
+                            <Input
+                                type="file"
+                                id="postFile"
+                                onChange= { handleFileChange }
+                                aria-describedby="imageUrl-helper-text"
+                            />
+                        <FormHelperText id="password-helper-text">
+                            Hit here to blow up the world<span role="img" aria-label="Happy">😄</span>
+                        </FormHelperText>
+                        <Button
+                            mt={4}
+                            bg="Black"
+                            color="White"
+                            type="submit"
+                            isLoading={ state }
+                        >
+                            Post
+                        </Button>
+                    </FormControl>
+            </Box>
+        )
+    }
+
