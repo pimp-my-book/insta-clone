b2fefd4f5e011a049d5b050d2d5819e8cfe35db6
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-19 15:36:32 +0200

    Login -> add imports

---
+++ insta-frontend/src/containers/Login.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import Auth from "@aws-amplify/auth";
+import { Box, Heading, Input, Button, FormControl, FormLabel, FormHelperText } from "@chakra-ui/core";
+import "../resources/styles/styles.css";
+
