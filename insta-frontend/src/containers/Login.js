b59aaf0a50233fe1c526cff22922795fa20876bd
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-19 15:37:24 +0200

    Login -> add login form

--- insta-frontend/src/containers/Login.js
+++ insta-frontend/src/containers/Login.js
@@ -18,3 +18,48 @@ import "../resources/styles/styles.css";
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
 
+    return(
+        <Box
+            display="block"
+            w="100%"
+            pr="35%"
+            pl="35%"
+        >
+            <form
+                onSubmit={ handleSubmit }
+            >
+                <Heading>Login</Heading>
+                    <FormControl size="md">
+                        <FormLabel>Email Address</FormLabel>
+                            <Input 
+                                type="email" 
+                                id="email"
+                                value={ email }
+                                onChange = { e => setEmail( e.target.value ) }
+                                aria-describedby="email-helper-text" />
+                        <FormHelperText id="email-helper-text">
+                            This is for your body<span role="img" aria-label="Happy">😄</span>
+                        </FormHelperText>
+                        <FormLabel>Password</FormLabel>
+                            <Input 
+                            type="password" 
+                            id="password"
+                            value={ password } 
+                            onChange = { e => setPassword( e.target.value ) }
+                            aria-describedby="password-helper-text" />
+                        <FormHelperText id="password-helper-text">
+                            This is for your soul<span role="img" aria-label="Happy">😄</span>
+                        </FormHelperText>
+                        <Button
+                            mt={4}
+                            bg="Black"
+                            color="White"
+                            type="submit"
+                        >
+                            Login
+                        </Button>
+                    </FormControl>
+            </form>
+        </Box>
+    )
+}
