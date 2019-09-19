129eaab10587e03dd4f628096b420f3456493038
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-19 15:33:55 +0200

    Add login container route

--- insta-frontend/src/Routes.js
+++ insta-frontend/src/Routes.js
@@ -4,10 +4,12 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Register from "./containers/Register";
 import CreatePost from "./containers/CreatePost";
+import Login from "./containers/Login";
 
 export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={ Home } props={ childProps } />
         <Route path="/Register" exact component={ Register } props={ childProps } />
         <Route path="/CreatePost" exact component={ CreatePost } props={ childProps } />
+        <Route path="/Login" exact component={ Login } props={ childProps } />
     </Switch>
