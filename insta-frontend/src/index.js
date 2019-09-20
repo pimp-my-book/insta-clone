e39910b468a25074e279dd570a2ed17b25f0949a
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-20 14:41:05 +0200

    Add inmemorycache + connectDevTools Apollo

--- insta-frontend/src/index.js
+++ insta-frontend/src/index.js
@@ -55,7 +55,8 @@ const httpLink = createHttpLink({
 
 const client = new ApolloClient({
     link: httpLink,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    connectToDevTools: true
 });
 
 ReactDOM.render(
